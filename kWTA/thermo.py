b284f658228bc63aaad4f0ffa5d45a7e12bb1020
a554b554 <wangliwu@zju.edu.cn>
2020-03-31 19:00:09 -0400

    Delete thermo.py

--- kWTA/thermo.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torchvision import datasets, transforms
-import torchvision
-from kWTA import models
-from kWTA import thermo
